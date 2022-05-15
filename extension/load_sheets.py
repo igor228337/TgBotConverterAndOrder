cd146aad0a868868fbe470ac6099e59878e256ae
Игорь <42812135+igor228337@users.noreply.github.com>
2022-05-15 14:23:28 +0300

    Update load_sheets.py

--- extension/load_sheets.py
+++ extension/load_sheets.py
@@ -18,19 +18,17 @@ spreadsheetId = ""  # сохраняем идентификатор файла
 async def load_sheets(data_user: list, data_order: list):
     service.spreadsheets().values().batchUpdate(spreadsheetId=spreadsheetId, body={
         "valueInputOption": "USER_ENTERED",
-        # Данные воспринимаются, как вводимые пользователем (считается значение формул)
         "data": [
             {"range": f"Users!A2:H{len(data_user) + 1}",
-             "majorDimension": "ROWS",  # Сначала заполнять строки, затем столбцы
+             "majorDimension": "ROWS",
              "values": data_user}
         ]
     }).execute()
     service.spreadsheets().values().batchUpdate(spreadsheetId=spreadsheetId, body={
         "valueInputOption": "USER_ENTERED",
-        # Данные воспринимаются, как вводимые пользователем (считается значение формул)
         "data": [
             {"range": f"Orders!A2:J{len(data_order) + 1}",
-             "majorDimension": "ROWS",  # Сначала заполнять строки, затем столбцы
+             "majorDimension": "ROWS",
              "values": data_order}
         ]
     }).execute()
